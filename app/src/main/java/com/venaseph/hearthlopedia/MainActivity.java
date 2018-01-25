d2efff221be3558df8e3fa20dacb63771b7ad88f
C.Peragine <cperagine@gmail.com>
2018-01-25 11:05:09 -0500

    Toolbar Added

--- app/src/main/java/com/venaseph/hearthlopedia/MainActivity.java
+++ app/src/main/java/com/venaseph/hearthlopedia/MainActivity.java
@@ -1,7 +1,9 @@
 package com.venaseph.hearthlopedia;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolBarSetup();
+
+    }
+
+    private void toolBarSetup() {
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("Hearthlopedia");
+        toolbar.setSubtitle("Subtitle");
+        toolbar.setLogo(R.drawable.hslogo);
+        toolbar.setTitleTextColor(Color.WHITE);
     }
 }
+
