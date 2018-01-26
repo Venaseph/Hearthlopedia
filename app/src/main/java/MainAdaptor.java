7af0e85863a03557ccf8af8865128c8d21f2ea9f
C.Peragine <cperagine@gmail.com>
2018-01-25 19:29:23 -0500

    Working on RecyclerView and Adaptor

---
+++ app/src/main/java/MainAdaptor.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Chris on 1/25/2018.
+ */
+
+public class MainAdaptor {
+}
--- app/src/main/java/com/venaseph/hearthlopedia/MainActivity.java
+++ app/src/main/java/com/venaseph/hearthlopedia/MainActivity.java
@@ -2,9 +2,17 @@ package com.venaseph.hearthlopedia;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager rvLayoutManager;
+    private RecyclerView.Adapter rvAdaptor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,12 +20,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolBarSetup();
+        recyclerViewSetup();
 
+    }
+
+    private void recyclerViewSetup() {
+        // Grab Recycler View
+        recyclerView = findViewById(R.id.recyclerView_Main);
+
+        //Assign Layout Manager to RecyclerView
+        rvLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(rvLayoutManager);
+
+        //Assign Adaptor to RecyclerView
+        //rvAdaptor = new
     }
 
     private void toolBarSetup() {
         //Needs work, frag/logo/search functionality
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         toolbar.setLogo(R.drawable.hslogo);
     }
 }
